cadf71cc5bbd0ff5bcdea3185b5e890d048caae8
Rushi <rushikeshkulkarni9212@gmail.com>
2022-01-21 14:57:47 +0530

    Comprehensive Assessment

--- springboot-backend-project/src/test/java/net/javaguides/springboot/SpringbootBackendProjectApplicationTests.java
+++ springboot-backend-project/src/test/java/net/javaguides/springboot/SpringbootBackendProjectApplicationTests.java
@@ -5,22 +5,20 @@ package net.javaguides.springboot;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import net.javaguides.springboot.controller.PatientController;
-
-
 import net.javaguides.springboot.model.Patient;
+import net.javaguides.springboot.repository.PatientsRepository;
 
 @SpringBootTest
 class SpringbootBackendProjectApplicationTests {
 
 	@Autowired
-	PatientController pRepo;
+    PatientsRepository pRepo;
 	
 	@Test
 	void contextLoads() {
@@ -31,12 +29,12 @@ class SpringbootBackendProjectApplicationTests {
 	
 	public void testCreate() {
 		Patient p1=new Patient();
-		p1.setId(130L);
-		p1.setName("Satyapriya");
-		p1.setVisitedDoctor("Dr.Ram Prasad");
-		p1.setDateofVisit("15-Jan-2022");
-		pRepo.createPatient(p1);
-		assertNotNull(pRepo.getPatientById(130L));
+		p1.setId(155L);
+		p1.setName("RK");
+		p1.setVisitedDoctor("Dr.Shubham");
+		p1.setDateofVisit("10-june-2021");
+		pRepo.save(p1);
+		assertNotNull(pRepo.getById(155L));
 		
 		
 	}
@@ -44,8 +42,28 @@ class SpringbootBackendProjectApplicationTests {
 	@Test
        public void testReadAll() {
 		
-		List<Patient> list = pRepo.getAllPatients();
+		List<Patient> list = pRepo.findAll();
 		assertThat(list).size().isGreaterThan(0);
 		
 	}
+	
+	@Test
+	public void UpdateTest() {
+		
+		Patient p1= new Patient();
+		p1=pRepo.findById(156L).get();
+		p1.setName("Virat Kohali");
+		pRepo.save(p1);
+		assertNotEquals("RK",pRepo.findById(156L).get().getName());
+	}
+	
+	@Test
+	public void deleteTest() {
+    
+	    pRepo.deleteById(157L);
+	    assertThat(pRepo.existsById(157L)).isFalse();
+	    
+	}
+	
+	
 }
